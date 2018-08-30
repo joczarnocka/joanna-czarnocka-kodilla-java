c3561bca1308b194a4a5be1b01910e7e3e3cf3eb
joczarnocka <jczarnocka@gmail.com>
2018-08-30 21:42:17 +0200

    moduł 7 zadanie 5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage(){
+        //Given
+        int[] numbers = new int[10];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+        numbers[5] = 60;
+        numbers[6] = 70;
+        numbers[7] = 80;
+        numbers[8] = 90;
+        numbers[9] = 100;
+
+        //When
+        double avg = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(55.0, avg,0.001);
+    }
+}
