05069bd4ed2e34f66be043068bba1afcc5bdc868
joczarnocka <jczarnocka@gmail.com>
2018-09-20 00:33:46 +0200

    zadanie 9.4 - poprawki

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/distribution/ExtraFoodShop.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/distribution/ExtraFoodShop.java
@@ -5,32 +5,27 @@ import java.util.Map;
 
 public class ExtraFoodShop implements Supplier {
 
-    private Map<Product,Double> productsAmounts;
+    private Map<Product, Double> productsAmounts;
 
-    public ExtraFoodShop(Map<Product,Double> productsAmounts){
-        this.productsAmounts= productsAmounts;
+    public ExtraFoodShop(Map<Product, Double> productsAmounts) {
+        this.productsAmounts = productsAmounts;
     }
 
     @Override
     public boolean process(OrderRequest orderRequest) {
         Product product = orderRequest.getProduct();
-        double amount;
-        for (Map.Entry<Product,Double> pram : productsAmounts.entrySet()){
-            if (pram.getKey().equals(product)){
-                amount = pram.getValue();
-                if (amount >= orderRequest.getAmount()){
-                    System.out.println("OK: zapasy w " + this.getClass().getSimpleName() + " dla produku " + product.getName() +  " większe niż zamówienie");
-                    amount -= orderRequest.getAmount();
-                    productsAmounts.remove(product);
-                    productsAmounts.put(product, amount);
-                    return true;
-                }
-                else {
-                    System.out.println("Problem: zapasy w " + this.getClass().getSimpleName()+ " dla produku " + product.getName() + " mniejsze niż zamówienie");
-                    return false;
-                }
-            }
+        double amount = productsAmounts.getOrDefault(product,0.0);
+
+        if (amount >= orderRequest.getAmount()) {
+            System.out.println("OK: zapasy w " + this.getClass().getSimpleName() + " dla produku " + product.getName() + " większe niż zamówienie");
+            amount -= orderRequest.getAmount();
+            productsAmounts.remove(product);
+            productsAmounts.put(product, amount);
+            return true;
+        } else {
+            System.out.println("Problem: zapasy w " + this.getClass().getSimpleName() + " dla produku " + product.getName() + " mniejsze niż zamówienie");
+            return false;
         }
-        return false;
     }
+
 }
